5baeb9c4a6c168e2370902e546f9659004fbbcd3
chocolate <readonlychocolate@gmail.com>
2025-02-22 09:39:53 +0800

    update utils

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,28 +1,29 @@
 export default {
 	hooked: {},
 
-	hookNotifis(unhook) {
-		if (unhook) {
-			if (!this.hooked.callSuccess)
-				return;
-
-			callSuccess = this.hooked.callSuccess;
-			delete this.hooked.callSuccess;
+	hookNotifis(t) {
+		if (t) {
+			this.hooked.callSuccess = callSuccess;
+			callSuccess = () => {};
 			return;
 		}
 
-		this.hooked.callSuccess = callSuccess;
-		callSuccess = () => {};
+		if (!this.hooked.callSuccess)
+			return;
+
+		callSuccess = this.hooked.callSuccess;
+		delete this.hooked.callSuccess;
 	},
 
 	hookCloseRight() {
-		const oldCloseRight = closeRight;
+		let oldCloseRight = closeRight;
 
 		closeRight = () => {
 			prepareRight(0);
 			userReload(1);
 
 			closeRight = oldCloseRight;
-		};	
+			oldCloseRight = null;
+		};
 	}
 };
